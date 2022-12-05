e8642a5d15f4fc963e3b8545e98eab1df3cee42d
v-lbei <FAREAST.v-lbei@microsoft.com>
2022-12-05 12:38:06 +0000

    update

--- fairseq/models/numerical_transformer_v2_lm.py
+++ fairseq/models/numerical_transformer_v2_lm.py
@@ -477,6 +477,10 @@ class TransformerDecoder(FairseqIncrementalDecoder):
                 - the decoder's features of shape `(batch, tgt_len, embed_dim)`
                 - a dictionary with any model-specific outputs
         """
+
+        if self.history is not None:
+            self.history.clean()
+            
         if alignment_layer is None:
             alignment_layer = self.num_layers - 1
 
